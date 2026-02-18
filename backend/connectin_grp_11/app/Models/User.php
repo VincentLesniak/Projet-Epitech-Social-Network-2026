073f0e9e7a19bcb965bd4f5f35358be1dc573b5f
Baussart Renaud <renaud.baussart@proton.me>
2026-02-18 17:07:03 +0100

    fix(User.php): add composant BelongsToMany

--- backend/connectin_grp_11/app/Models/User.php
+++ backend/connectin_grp_11/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -49,4 +50,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function likedPosts(): BelongsToMany
+{
+    return $this->belongsToMany(Post::class, 'liked', 'user_id', 'post_id')
+                ->withTimestamps()
+                ->updatedAt(null);
+}
 }
