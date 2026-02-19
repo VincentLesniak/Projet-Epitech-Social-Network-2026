229b2c278d3ca4df7315941e2c4da55c9d847a0e
MaximeMDev <maximemortelec@gmail.com>
2026-02-19 12:42:09 +0100

    feat:header

---
+++ frontend/src/Log.jsx
@@ -0,0 +1,11 @@
+import Title from "./components/Title";
+
+function Log() {
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <Title />
+    </div>
+  );
+}
+
+export default Log;
---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <h1>
+      <a href="#">Connect'in</a>
+    </h1>
+  );
+};
+
+export default Title;
