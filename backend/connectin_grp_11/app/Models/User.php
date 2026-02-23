a01b77974490c98c41b1e9f2949cbe050dc25781
Baussart Renaud <renaud.baussart@proton.me>
2026-02-23 10:49:48 +0100

    fix: change liking method in model

--- backend/connectin_grp_11/app/Models/User.php
+++ backend/connectin_grp_11/app/Models/User.php
@@ -55,8 +55,9 @@ class User extends Authenticatable
     // Relation Many-to-Many avec les Groupes via la table 'user_groups'
     public function likedPosts(): BelongsToMany
     {
+        // On indique que cette relation utilise la table 'liked' avec 'user_id' et 'post_id' comme clés étrangères 
+        // (user est récupéré via auth()->user() dans le controller et post via la route model binding)
         return $this->belongsToMany(Post::class, 'liked', 'user_id', 'post_id')
-            ->withTimestamps()
-            ->updatedAt(null);
+            ->withTimestamps();
     }
 }
