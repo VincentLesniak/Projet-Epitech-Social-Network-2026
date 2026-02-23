a681dab9df9c4fbc954cba7c80b45075a832e47c
Vincent Lesniak <vincentlesniakpro@gmail.com>
2026-02-23 16:43:38 +0100

    feat: logout feature tested with postman

--- backend/connectin_grp_11/routes/api.php
+++ backend/connectin_grp_11/routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LikeController;
@@ -6,3 +7,11 @@ use App\Http\Controllers\LikeController;
 Route::post('/posts/{post}/like', [LikeController::class, 'toggle'])->middleware('auth:sanctum');
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+
+// Le groupe protégé par "auth:sanctum", tout ce qui se fait avec la nécessité d'être connécté se fait ici
+Route::middleware('auth:sanctum')->group(function () {
+    
+    route::post('/logout', [AuthController::class, 'logout']);
+
+});
